61667d37fe2084dfc5074a0e97704a23a70fc6ca
Cavin Raj <cavinraj96@gmail.com>
2026-01-07 21:12:04 +0800

    Completed Clock Out page and linked it to Dashboard Main Menu page and also removed some unused useless import declarations in the files

--- src/ClockInPage.java
+++ src/ClockInPage.java
@@ -6,7 +6,6 @@ import java.io.PrintWriter;
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
-import java.util.ArrayList;
 import java.util.Scanner;
 
 import javax.swing.JFrame;
