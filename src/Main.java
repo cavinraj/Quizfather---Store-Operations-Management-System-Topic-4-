f7bd588b238e6927e464fa7f4e8fbf477459263f
Raihan Quddus Tamir <54447556+raihan1205@users.noreply.github.com>
2026-01-08 05:38:58 +0800

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,16 @@
-import java.util.*;
-
+package src;
 public class Main {
     public static void main(String[] args) {
 
-        List<Employee> employees =
-                DataLoader.loadEmployees("employee.csv");
-
-        List<Outlet> outlets =
-                DataLoader.loadOutlets("outlet.csv");
-
-        List<Model> models =
-                DataLoader.loadModels("model.csv");
+        // DATA LOAD STATE
+        AppData.init();
 
         System.out.println("System started successfully.");
-        System.out.println("Employees loaded: " + employees.size());
-        System.out.println("Outlets loaded: " + outlets.size());
-        System.out.println("Models loaded: " + models.size());
+        System.out.println("Employees loaded: " + AppData.employees.size());
+        System.out.println("Outlets loaded: " + AppData.outlets.size());
+        System.out.println("Models loaded: " + AppData.models.size());
+
+        // Start login page (dashboard comes AFTER login)
+        new LoginPage();
     }
 }
