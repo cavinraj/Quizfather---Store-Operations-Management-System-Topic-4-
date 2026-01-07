9d1c59e3cb9b687d336009ab4c098f4421969480
Raihan Quddus Tamir <54447556+raihan1205@users.noreply.github.com>
2026-01-08 05:37:59 +0800

    Update AppData.java

--- src/AppData.java
+++ src/AppData.java
@@ -1,12 +1,15 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-
-/**
- *
- * @author Raihan Quddus Tamir
- */
+package src;
+import java.util.*;
 public class AppData {
-    
+    // Shared data across the whole system
+    public static List<Employee> employees;
+    public static List<Outlet> outlets;
+    public static List<Model> models;
+
+    // Load data ONCE when system starts
+    public static void init() {
+        employees = DataLoader.loadEmployees("employee.csv");
+        outlets   = DataLoader.loadOutlets("outlet.csv");
+        models    = DataLoader.loadModels("model.csv");
+    }
 }
