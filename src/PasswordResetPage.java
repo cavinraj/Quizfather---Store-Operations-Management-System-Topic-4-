2129ed1f1f9012f53bc3eb3f1bc1fc86856b9b1c
Cavin Raj <cavinraj96@gmail.com>
2026-01-08 20:21:36 +0800

    Tidied up codes in PasswordResetPage

--- src/PasswordResetPage.java
+++ src/PasswordResetPage.java
@@ -53,12 +53,15 @@ public class PasswordResetPage extends JFrame implements ActionListener{
 
     @Override
     public void actionPerformed(ActionEvent e){
-        boolean match = false;
         String first_password_text = String.valueOf(enter_password_text.getPassword());
         String second_password_text = String.valueOf(confirm_password_text.getPassword());
         String user_id = "";
         String filename = "data/employee.csv";
 
+        handle_reset_password(filename, first_password_text, second_password_text, user_id);
+    }
+
+    private void handle_reset_password(String filename,String first_password_text,String second_password_text,String user_id){
         if (first_password_text.isEmpty() || second_password_text.isEmpty()) {
             JOptionPane.showMessageDialog(this, "New Password must not be empty!");
             return;
